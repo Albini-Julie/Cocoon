c9488cdbab04fc41807b40b893d06ded4c46d96a
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-26 21:51:20 +0200

    création et remplissage du fichier flechedroite

---
+++ src/components/icons/flechedroite.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    width="23"
+    height="24"
+    viewBox="0 0 23 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="relative h-6 w-[22px] flex-shrink-0 flex-grow-0"
+    preserveAspectRatio="none"
+  >
+    <path
+      d="M16.0833 8L19.75 12M19.75 12L16.0833 16M19.75 12L3.25 12"
+      stroke="white"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
