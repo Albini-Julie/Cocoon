878edab508bf21252f14069f2663be460c7fac8a
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-26 15:53:17 +0200

    mise en place des routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
+import ArtistesView from '../views/ArtistesView.vue'
+import ArtisteView from '../views/ArtisteView.vue'
+import ConcertView from '../views/ConcertView.vue'
+import ContactView from '../views/ContactView.vue'
+import FestivalView from '../views/FestivalView.vue'
+import MentionslegalesView from '../views/MentionslegalesView.vue'
+import ProgrammationView from '../views/ProgrammationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Accueil', component: AccueilView },
+    { path: '/', name: 'Artistes', component: ArtistesView },
+    { path: '/', name: 'Artiste', component: ArtisteView },
+    { path: '/', name: 'Concert', component: ConcertView },
+    { path: '/', name: 'Contact', component: ContactView },
+    { path: '/', name: 'Festival', component: FestivalView },
+    { path: '/', name: 'Mentionslegales', component: MentionslegalesView },
+    { path: '/', name: 'Programmation', component: ProgrammationView },
     // ici les autre routes
   ]
 })
