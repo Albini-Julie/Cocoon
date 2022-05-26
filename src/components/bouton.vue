0900b93a6923ab6742006927287f0d77aa9402c4
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-26 20:02:23 +0200

    création bouton

--- src/components/bouton.vue
+++ src/components/bouton.vue
@@ -1,9 +1,17 @@
-<template></template>
+<template>
+  <div class="flex w-max gap-2 rounded-lg bg-indigo-800 p-5">
+    <router-link class="font-work-sans font-semibold text-white" to="Festival">{{ texte }}</router-link>
+    <flechedroite />
+  </div>
+</template>
 
 <script>
-import { defineComponent } from "@vue/composition-api";
+import flechedroite from "../components/icons/flechedroite.vue";
 
 export default {
+  components: {
+    flechedroite,
+  },
   props: {
     texte: String,
   },
