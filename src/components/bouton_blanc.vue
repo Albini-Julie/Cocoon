ece2b00e0128baf19e91ab8c6a61d184406206f1
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-27 18:26:44 +0200

    création du bouton blanc

---
+++ src/components/bouton_blanc.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex gap-2 rounded-lg border-2 border-blue-700 bg-white p-5">
+    <router-link class="font-work-sans font-semibold text-blue-700" to="Festival">{{ texte }}</router-link>
+    <flechedroiteBleu />
+  </div>
+</template>
+
+<script>
+import flechedroiteBleu from "../components/icons/flechedroiteBleu.vue";
+
+export default {
+  components: {
+    flechedroiteBleu,
+  },
+  props: {
+    texte: String,
+  },
+};
+</script>
