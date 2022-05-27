088ca449b9408c295f80a265b1900da0ba780b33
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-27 12:46:14 +0200

    début de création de card

---
+++ src/components/card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-5 flex flex-col items-center">
+    <img class="rounded-xl" :src="image" alt="image concert" />
+    <h4 class="mt-5 font-work-sans font-semibold">{{ titre }}</h4>
+    <p class="m-2 font-work-sans text-blue-700">{{ genre }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    presentation: String,
+    genre: String,
+    titre: String,
+  },
+};
+</script>
