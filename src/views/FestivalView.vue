8fcff96c3d76e39ce60f2ef8e89b55c9a5b8a94b
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-27 18:28:00 +0200

    création de la page Festival

---
+++ src/views/FestivalView.vue
@@ -0,0 +1,130 @@
+<template>
+  <header class="mb-16">
+    <HeaderMobile />
+  </header>
+
+  <div class="flex flex-col items-center">
+    <h3 class="mx-2 text-center font-work-sans text-3xl font-semibold">Découvrez un festival d’un nouveau genre :</h3>
+    <img class="mt-5 w-2/4" src="../../public/grand-logo.png" alt="logo Cocoon" />
+  </div>
+
+  <h4 class="work-sans mt-16 mb-2 ml-2 text-xl font-bold">Le concept</h4>
+  <div class="flex">
+    <p class="ml-2 font-work-sans text-[14px]">
+      Cocoon est un festival unique qui vous propose une expérience inédite : allez dans un monde virtuel pour voir vos artistes préférés en
+      concert ! Plus besoin de vous déplacer, de trouver du temps pour vous rendre sur le lieu du concert, aujourd’hui c’est le concert qui
+      vient à vous !
+    </p>
+    <img class="ml-5 h-1/4 w-2/4" src="../../public/Libre_Droit/joueuse.jpg" alt="utilisatrice casque de réalité virtuelle" />
+  </div>
+  <p class="mx-2 mt-5 font-work-sans text-[14px]">
+    Lorsque vous allez dans le festival, vous arrivez dans un univers unique virtuel : créez votre avatar et ayez à votre disposition :
+    boutiques, salons de discussion, rediffusions et bien sûr des concerts ! Chaque jour, 20 nouveaux concerts du monde entier !
+  </p>
+
+  <h4 class="work-sans mt-16 mb-2 ml-2 text-xl font-bold">L’histoire du projet</h4>
+  <div class="mt-5 flex items-center">
+    <img class="mr-5 h-1/4 w-2/4" src="../../public/Libre_Droit/3d.jpg" alt="utilisatrice casque de réalité virtuelle" />
+    <div class="flex flex-col">
+      <p class="mr-2 font-work-sans text-[14px]">
+        Cocoon est né pour contrer l’effet destructeur du confinement sur le monde de la culture. En effet, le public étant bloqué chez lui,
+        et vu la situation sanitaire de l’époque, toutes les représentations quelles qu’elles soient ne pouvaient avoir lieu.
+      </p>
+      <p class="mr-2 font-work-sans text-[14px]">
+        Cocoon a donc voulu permettre de mettre en lien les artistes et leur public dans un monde à part pour les faire se retrouver.
+      </p>
+    </div>
+  </div>
+  <p class="mx-2 mt-5 font-work-sans text-[14px]">
+    Suite à l’engouement du public pour la première édition du concept de Cocoon, le projet a continué d’évoluer avec une édition 2, puis 3.
+  </p>
+
+  <h3 class="mx-2 mt-5 text-center font-work-sans text-3xl font-semibold">Les avantages de Cocoon</h3>
+  <div class="ml-[5rem] mt-5 flex items-center">
+    <img class="mr-2" src="../../public/numero1.jpg" alt="numéro 1" />
+    <div class="flex flex-col">
+      <h4 class="work-sans ml-2 text-xl font-bold">Économie</h4>
+      <p class="mx-2 mt-5 font-work-sans text-[14px]">
+        Avec Cocoon, pas de coûts de transport, de baby-sitting, ou de restauration sur place !
+      </p>
+    </div>
+  </div>
+
+  <div class="mr-[5rem] mt-5 flex items-center">
+    <div class="flex flex-col">
+      <h4 class="work-sans ml-2 mb-5 text-xl font-bold">Gain de temps</h4>
+      <div class="flex flex-col">
+        <p class="font-work-sans text-[14px]">
+          Pas de perte de temps sur les routes : mettez votre casque et l’expérience peut commencer !
+        </p>
+        <p class="font-work-sans text-[14px]">
+          A la fin du spectacle : pas besoin de chercher sa voiture et d’entamer les bouchons : enlevez votre casque et vous êtes chez vous
+          !
+        </p>
+      </div>
+    </div>
+    <img class="mr-2 ml-2" src="../../public/numero2.jpg" alt="numéro 2" />
+  </div>
+
+  <div class="ml-[5rem] mt-5 flex items-center">
+    <img class="mr-2" src="../../public/numero3.jpg" alt="numéro 3" />
+    <div class="flex flex-col">
+      <h4 class="work-sans ml-2 text-xl font-bold">Social</h4>
+      <p class="mx-2 mt-5 font-work-sans text-[14px]">
+        Grâce à Cocoon, rencontrez des gens qui partagent la même passion que vous plus facilement : les avatars vous rendent anonymes et
+        vous permettent donc de plus facilement rentrer en contact avec autrui !
+      </p>
+    </div>
+  </div>
+
+  <div class="relative h-[863px] w-[375px]">
+    <img src="../../public/Libre_Droit/joueuse3.jpg" class="absolute left-[-1px] top-0 h-36 w-[158px] object-cover" /><img
+      src="../../public/Libre_Droit/concert1.jpg"
+      class="absolute left-[157px] top-[51px] h-36 w-[158px] object-cover"
+    /><img src="../../public/Libre_Droit/concert12.jpg" class="absolute left-[157px] top-[-1px] h-[52px] w-[217px] object-cover" /><img
+      src="../../public/Libre_Droit/concert8.jpg"
+      class="absolute left-[315px] top-[51px] h-56 w-[59px] object-none"
+    /><img src="../../public/Libre_Droit/concert7.jpg" class="absolute left-[-1px] top-[143px] h-[124px] w-[79px] object-cover" /><img
+      src="../../public/Libre_Droit/concert4.jpg"
+      class="absolute left-[78px] top-[143px] h-[178px] w-[79px] object-cover"
+    /><img src="../../public/Libre_Droit/joueur.jpg" class="absolute left-[157px] top-[194px] h-[189px] w-[158px] object-none" /><img
+      src="../../public/Libre_Droit/femme.jpg"
+      class="absolute left-[-1px] top-[267px] h-[54px] w-[79px] object-cover"
+    /><img src="../../public/Libre_Droit/concert6.jpg" class="absolute left-[-1px] top-80 h-[158px] w-[158px] object-cover" /><img
+      src="../../public/Libre_Droit/concert5.jpg"
+      class="absolute left-[315px] top-[275px] h-[108px] w-[59px] object-cover"
+    /><img src="../../public/Libre_Droit/concert11.jpg" class="absolute left-[157px] top-[381px] h-[135px] w-[217px] object-cover" /><img
+      src="../../public/Libre_Droit/joueuse2.jpg"
+      class="absolute left-[-1px] top-[478px] h-[97px] w-[158px] object-cover"
+    /><img src="../../public/Libre_Droit/concert3.jpg" class="absolute left-[236px] top-[516px] h-52 w-[138px] object-cover" /><img
+      src="../../public/Libre_Droit/concert9.jpg"
+      class="absolute left-[157px] top-[516px] h-[165px] w-[79px] object-cover"
+    /><img src="../../public/Libre_Droit/platine.jpg" class="absolute left-[-1px] top-[675px] h-[49px] w-[237px] object-cover" /><img
+      src="../../public/Libre_Droit/concert10.jpg"
+      class="absolute left-[-1px] top-[575px] h-[100px] w-[158px] object-cover"
+    /><img src="../../public/Libre_Droit/concert2.jpg" class="absolute left-[-1px] top-[724px] h-[110px] w-[237px] object-none" /><img
+      src="../../public/Libre_Droit/joueur3.jpg"
+      class="absolute left-[236px] top-[721px] h-[141px] w-[138px] object-none"
+    />
+  </div>
+
+  <footerblanc class="mt-16" />
+</template>
+
+
+
+
+
+  <script>
+import HeaderMobile from "../components/HeaderMobile.vue";
+import card from "../components/card.vue";
+import footerblanc from "../components/FooterBlanc.vue";
+
+export default {
+  components: {
+    HeaderMobile,
+    card,
+    footerblanc,
+  },
+};
+</script>
