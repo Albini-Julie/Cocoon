29c15ebe3b22cef1beab9dd3e941a4b7075b23fd
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-27 18:43:49 +0200

    création page contact

---
+++ src/views/ContactView.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="bg-gray-900">
+    <HeaderNoir />
+  </header>
+</template>
+
+<script>
+import HeaderNoir from "../components/HeaderMobile_noir.vue";
+
+export default {
+  components: {
+    HeaderNoir,
+  },
+};
+</script>
