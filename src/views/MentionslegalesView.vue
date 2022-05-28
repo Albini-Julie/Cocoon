f1a23980c0dd6d00a68b751fd313cd98b0d24b05
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-29 00:08:52 +0200

    debut remplissage mentions legales

---
+++ src/views/MentionslegalesView.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="mb-16">
+    <HeaderMobile />
+  </header>
+
+  <h3 class="mx-2 text-center font-work-sans text-3xl font-semibold">Découvrez un festival d’un nouveau genre :</h3>
+  <p class="mt-5 mb-10 text-center font-work-sans text-[14px]">Voici un aperçu de tout l’aspect légal de notre site web</p>
+</template>
+
+
+<script>
+import HeaderMobile from "../components/HeaderMobile.vue";
+
+export default {
+  components: {
+    HeaderMobile,
+  },
+};
+</script>
