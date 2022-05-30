70491f96acc0c26f1e9160373a55e0621d60245b
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-30 15:49:40 +0200

    création  de paramètres communs

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template >
-  <RouterView />
+  <div
+    :class="
+      $route.name === 'Artiste' || $route.name === 'Concert' || $route.name === 'Contact' ? 'bg-gray-900 text-white' : 'bg-white text-black'
+    "
+  >
+    <RouterView />
+  </div>
 </template>
 
 <script>
