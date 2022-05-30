6127c17943f827d6c941e6f89030c0b8870ce909
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-30 15:52:30 +0200

    création et remplissage chevron

---
+++ src/components/icons/chevronBlanc.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="21"
+    height="21"
+    viewBox="0 0 21 21"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="relative h-5 w-5 flex-shrink-0 flex-grow-0"
+    preserveAspectRatio="none"
+  >
+    <path d="M8.25 4.66668L14.0833 10.5L8.25 16.3333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+  </svg>
+</template>
