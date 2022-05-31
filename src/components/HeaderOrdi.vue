0cee8f4994b44c39dc98d88642af6d5d63109a16
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-05-31 17:56:05 +0200

    ajout des routes

--- src/components/HeaderOrdi.vue
+++ src/components/HeaderOrdi.vue
@@ -2,7 +2,14 @@
   <div class="m-5 flex items-center gap-10">
     <router-link to="/">
       <img
-        v-if="$route.name === 'Artiste' || $route.name === 'Concert' || $route.name === 'Contact' || $route.name === 'CreateArtiste'"
+        v-if="
+          $route.name === 'Artiste' ||
+          $route.name === 'Concert' ||
+          $route.name === 'Contact' ||
+          $route.name === 'CreateArtiste' ||
+          $route.name === 'UpdateArtiste' ||
+          $route.name === 'DeleteArtiste'
+        "
         src="../../public/LogoSimpleGrandBlanc.svg"
         alt="logo Cocoon" />
       <img v-else src="../../public/LogoSimpleGrandNoir.svg" alt="logo Cocoon"
@@ -12,28 +19,56 @@
         <div class="flex gap-2">
           <li><router-link to="/Programmation">Programmation</router-link></li>
           <chevronBlanc
-            v-if="$route.name === 'Artiste' || $route.name === 'Concert' || $route.name === 'Contact' || $route.name === 'CreateArtiste'"
+            v-if="
+              $route.name === 'Artiste' ||
+              $route.name === 'Concert' ||
+              $route.name === 'Contact' ||
+              $route.name === 'CreateArtiste' ||
+              $route.name === 'UpdateArtiste' ||
+              $route.name === 'DeleteArtiste'
+            "
           />
           <chevron v-else />
         </div>
         <div class="flex gap-2">
           <li><router-link to="/Artistes">Artistes</router-link></li>
           <chevronBlanc
-            v-if="$route.name === 'Artiste' || $route.name === 'Concert' || $route.name === 'Contact' || $route.name === 'CreateArtiste'"
+            v-if="
+              $route.name === 'Artiste' ||
+              $route.name === 'Concert' ||
+              $route.name === 'Contact' ||
+              $route.name === 'CreateArtiste' ||
+              $route.name === 'UpdateArtiste' ||
+              $route.name === 'DeleteArtiste'
+            "
           />
           <chevron v-else />
         </div>
         <div class="flex gap-2">
           <li><router-link to="/Festival">Festival</router-link></li>
           <chevronBlanc
-            v-if="$route.name === 'Artiste' || $route.name === 'Concert' || $route.name === 'Contact' || $route.name === 'CreateArtiste'"
+            v-if="
+              $route.name === 'Artiste' ||
+              $route.name === 'Concert' ||
+              $route.name === 'Contact' ||
+              $route.name === 'CreateArtiste' ||
+              $route.name === 'UpdateArtiste' ||
+              $route.name === 'DeleteArtiste'
+            "
           />
           <chevron v-else />
         </div>
         <div class="flex gap-2">
           <li><router-link to="/Contact">Contact</router-link></li>
           <chevronBlanc
-            v-if="$route.name === 'Artiste' || $route.name === 'Concert' || $route.name === 'Contact' || $route.name === 'CreateArtiste'"
+            v-if="
+              $route.name === 'Artiste' ||
+              $route.name === 'Concert' ||
+              $route.name === 'Contact' ||
+              $route.name === 'CreateArtiste' ||
+              $route.name === 'UpdateArtiste' ||
+              $route.name === 'DeleteArtiste'
+            "
           />
           <chevron v-else />
         </div>
