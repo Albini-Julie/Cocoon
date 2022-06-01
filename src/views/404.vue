9d1a4a76ea11de3b0282f93d6f4ba58c8a8ee4cc
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-06-01 16:48:11 +0200

    finalisation 404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="-mt-5 dark:bg-black dark:text-white">
     <header class="mb-16">
-      <div class="md:hidden"><Header /></div>
+      <div class="md:hidden"><HeaderMobile /></div>
       <div class="hidden md:block">
         <HeaderOrdi />
       </div>
@@ -9,7 +9,7 @@
 
     <div class="flex flex-col justify-center">
       <div class="flex justify-center">
-        <h2 class="font-work-sans text-5xl font-semibold">Page introuvable</h2>
+        <h2 class="text-center font-work-sans text-5xl font-semibold">Page introuvable</h2>
       </div>
       <p class="mx-5 mt-5 mb-20 text-center font-work-sans text-white">
         Nous sommmes désolés mais la page que vous demandez ne semble pas exister ! Redirigez-vous vers une page connue de Cocoon via la
@@ -17,33 +17,42 @@
       </p>
     </div>
 
-    <form class="flex justify-center gap-10" action="https://duckduckgo.com/" method="GET" x-data="{ q:'', }" x-on:submit="q = q'">
+    <form class="justify-center sm:flex" action="https://duckduckgo.com/" method="GET" x-data="{ q:'', }" x-on:submit="q = q'">
       <input
-        class="border-b-2 border-gray-900 border-b-white bg-gray-900 text-white dark:bg-black"
+        class="m-5 border-b-2 border-gray-900 border-b-white bg-gray-900 text-white dark:bg-black"
         placeholder="Votre recherche"
         type="text"
         id="q"
         name="q"
         x-model="q"
       />
-      <bouton class="mr-10 w-max">Search</bouton>
+      <button class="ml-10 mt-5 flex w-max gap-2 rounded-lg bg-indigo-800 p-5 font-work-sans font-semibold text-white" type="submit">
+        Search
+        <flechedroite />
+      </button>
     </form>
 
-    <h3 class="mt-10 ml-5 font-work-sans text-xl font-semibold">Plan du site</h3>
-    <div class="flex flex-col">
-      <router-link to="/" class="mt-5 ml-5 font-work-sans text-lg font-medium">Accueil</router-link>
-      <div class="flex">
-        <router-link to="/Artistes" class="mt-5 ml-5 font-work-sans text-lg font-medium">Artistes</router-link>
-        <chevron />
-        <router-link to="/Artiste" class="mt-5 ml-5 font-work-sans text-lg font-medium">Artiste</router-link>
+    <div class="flex flex-col items-center">
+      <h3 class="mt-10 ml-5 font-work-sans text-2xl font-semibold">Plan du site</h3>
+      <div class="flex flex-col">
+        <router-link to="/" class="my-5 ml-5 font-work-sans text-lg font-medium">Accueil</router-link>
+        <div class="flex items-center gap-5">
+          <router-link to="/Artistes" class="ml-5 font-work-sans text-lg font-medium">Artistes</router-link>
+          <chevron class="items-end" />
+          <router-link to="/Artiste" class="font-work-sans text-lg font-medium">Artiste</router-link>
+        </div>
+        <router-link to="/Contact" class="mt-5 ml-5 font-work-sans text-lg font-medium">Contact</router-link>
+        <router-link to="/Mentionslegales" class="my-5 ml-5 font-work-sans text-lg font-medium">Mentions légales</router-link>
+        <div class="flex items-center gap-5">
+          <router-link to="/Programmation" class="ml-5 font-work-sans text-lg font-medium">Programmation</router-link>
+          <chevron class="items-end" />
+          <router-link to="/Concert" class="font-work-sans text-lg font-medium">Concert</router-link>
+        </div>
+        <router-link to="/Festival" class="mt-5 ml-5 font-work-sans text-lg font-medium">Festival</router-link>
       </div>
-      <router-link to="/Concert" class="mt-5 ml-5 font-work-sans text-lg font-medium">Concert</router-link>
-      <router-link to="/Contact" class="mt-5 ml-5 font-work-sans text-lg font-medium">Contact</router-link>
-      <router-link to="/Mentionslegales" class="mt-5 ml-5 font-work-sans text-lg font-medium">Mentions légales</router-link>
-      <router-link to="/Programmation" class="mt-5 ml-5 font-work-sans text-lg font-medium">Programmation</router-link>
-      <router-link to="/Festival" class="mt-5 ml-5 font-work-sans text-lg font-medium">Festival</router-link>
     </div>
   </main>
+  <Footer class="mt-16" />
 </template>
 
 
@@ -53,6 +62,7 @@ import HeaderOrdi from "../components/HeaderOrdi.vue";
 import Footer from "../components/Footer.vue";
 import bouton from "../components/bouton.vue";
 import chevron from "../components/icons/chevronBlanc.vue";
+import flechedroite from "../components/icons/flechedroite.vue";
 
 export default {
   components: {
@@ -61,6 +71,7 @@ export default {
     Footer,
     bouton,
     chevron,
+    flechedroite,
   },
 };
 </script>
